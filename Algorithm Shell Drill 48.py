6668c52bc78068861f1e5b484834cde4503f795a
CJJones1962 <cjcarringtonjones@hotmail.com>
2017-06-05 12:49:06 -0700

    Python Drill 48

---
+++ Algorithm Shell Drill 48.py
@@ -0,0 +1,13 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: C:/Users/HP/Desktop/CJ Python/Algorithm.py ============
+>>> a = [67,45,2,13,1,998]
+>>> InsertionSort (a)
+>>> a
+[1, 2, 13, 45, 67, 998]
+>>> b = [89,23,33,45,10,12,45,45,45]
+>>> InsertionSort (b)
+>>> b
+[10, 12, 23, 33, 45, 45, 45, 45, 89]
+>>> 
